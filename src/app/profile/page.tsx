b2943a0e0d8ec07305d3e04dc4c3521528b605fb
804115 <54371378+804115@users.noreply.github.com>
2024-01-20 11:47:31 -0800

    ass

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+export default function Page(){
+    return(
+        <Box display="flex" flexDirection="column" alignItems="stretch" padding={1}>
+            <Button variant="outlined">Outlined</Button>
+        </Box>
+    );
+}
+
