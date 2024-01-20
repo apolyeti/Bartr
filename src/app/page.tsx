cb8f1c45b410a4e94698de5e55719aad397e448c
804115 <jkoo9@ucsc.edu>
2024-01-20 13:26:58 -0800

    im in hell

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 import Image from "next/image";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
 
 export default function Home() {
   return (
-    <main>
-
-      <div>
+      <Container maxWidth={false} sx={{ backgroundColor: "black", marginTop: "2rem 0", width: "100%"}}>
         <Image
           src= "/bartrlogo.svg"
-          alt="BartrLogo"
-          width={150}
-          height={37}
+          alt="logo"
+          width={400}
+          height={80}
         />
-      </div>
-
-    </main>
+        
+        <Stack spacing={2} direction="row">
+          <Button variant="contained">LOG IN</Button>
+          <Button variant="outlined">SIGN UP</Button>
+        </Stack>
+      </Container>
   );
 }
