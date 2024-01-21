3d1e477831e76713b88890671d65c8b0a4ba6213
apolyeti <azhandarveen@gmail.com>
2024-01-21 06:49:46 -0800

    remove item api

---
+++ src/app/api/removeitem/route.ts
@@ -0,0 +1,19 @@
+import prisma from "@/utils/db";
+
+export async function POST(req: Request, res: Response) {
+    // body will consist of post id
+    const body = await req.json();
+    const postId = body.postId;
+
+    // find post by id and remove it
+    
+    const post = await prisma.post.delete({
+        where: {
+            id: postId,
+        },
+    });
+    console.log('this is post')
+    console.log(post);
+
+    return Response.json({ success: true })
+}
