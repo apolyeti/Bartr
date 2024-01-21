10d2a0dbc1a0b8224238e000c14f53a529f951a9
Nicholas <mo.nicholas@gmail.com>
2024-01-20 20:40:25 -0800

    fixed not-found page, centered and split 404 and not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,3 @@
-// 404 page
-// ----------------------------------------------------------------------
 import { Box, Typography, Container, Grid } from '@mui/material';
 
 export default function NotFoundPage() {
@@ -7,9 +5,10 @@ export default function NotFoundPage() {
         <Container>
             <Grid container spacing={10}>
                 <Grid item xs={12}>
-                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-                        <Typography variant="h1" component="h1" gutterBottom>
-                            404 | Page Not Found
+                    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', textAlign: 'center'}}>
+                        {/* Combined text with line break */}
+                        <Typography variant="h1" component="h1" sx={{whiteSpace: 'pre-line' }}>
+                            404{'\n'}Page&nbsp;Not&nbsp;Found
                         </Typography>
                     </Box>
                 </Grid>
