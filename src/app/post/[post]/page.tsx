d5d1737040d0811a1b1f3628014c6cd77badd28c
Hovhannes Muradyan <76452706+Yelloo5191@users.noreply.github.com>
2024-01-20 19:08:56 -0800

    post page

--- src/app/post/[post]/page.tsx
+++ src/app/post/[post]/page.tsx
@@ -1,18 +1,18 @@
-import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 import prisma from "@/utils/db";
-import { cache } from 'react';
 import { notFound } from 'next/navigation';
 import ItemPost from '@/components/ItemComponents/ItemPost';
+import { Box, Container, Typography } from '@mui/material';
 
 export default async function Page({ params }: { params: { post: string } }) {
     
+    let userId;
     const post = await prisma.post.findUniqueOrThrow({
         where: {
           id: parseInt(params.post),
         }}).then((post) => {
+            userId = post.authorId;
             return {
                 id: post.id,
                 title: post.title,
@@ -22,10 +22,21 @@ export default async function Page({ params }: { params: { post: string } }) {
             // make 404 page
             return notFound();
         });
+    const user = await prisma.user.findUnique({
+        where: {
+          id: userId,
+        },
+    }) || {name: "User not found"};
 
     return (
-      <Stack>
-        <ItemPost item={post} />
-      </Stack>
+        <Container>
+            <Stack direction="row">
+                <ItemPost item={post} />
+                <Box>
+                    <Typography fontFamily="monospace" variant="h5">{user.name}</Typography>
+                    <Typography fontFamily="monospace" variant="h6">{post.content}</Typography>
+                </Box>
+            </Stack>
+        </Container>
     ); 
 }
--- src/components/ItemComponents/ItemPost.tsx
+++ src/components/ItemComponents/ItemPost.tsx
@@ -5,6 +5,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import { Box, CardMedia, Typography } from "@mui/material";
 
+import NextLink from 'next/link';
 
 
 interface ItemPostProps {
@@ -20,32 +21,34 @@ let location = "College 9";
 
 export default function ItemPost({ item }: ItemPostProps) {
     return (
-        <Card
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                width: 300,
-                height: 400,
-                margin: 2,
-                boxShadow: 0
-            }}
-        >
-            <CardMedia
-                image="https://cdn.discordapp.com/attachments/1157200811282157598/1198424995077034054/hat.jpeg?ex=65bedb40&is=65ac6640&hm=004d01afe509f01244eb7c4beabe70ff95580962ee98d8a14e8ceb94d3ac32b3&"
-                component="img"
+        <NextLink href={`/post/${item.id}`}>
+            <Card
                 sx={{
-                    width: 275,
-                    height: 300,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    width: 300,
+                    height: 400,
+                    margin: 2,
+                    boxShadow: 0
                 }}
-            />
-            <CardContent sx={{height: "25%", margin: 0}}>
-                <Typography gutterBottom variant="h5" component="div">
-                    {item.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    {location}
-                </Typography>
-            </CardContent>
-        </Card>
+            >
+                <CardMedia
+                    image="https://cdn.discordapp.com/attachments/1157200811282157598/1198424995077034054/hat.jpeg?ex=65bedb40&is=65ac6640&hm=004d01afe509f01244eb7c4beabe70ff95580962ee98d8a14e8ceb94d3ac32b3&"
+                    component="img"
+                    sx={{
+                        width: 275,
+                        height: 300,
+                    }}
+                />
+                <CardContent sx={{height: "25%", margin: 0}}>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {item.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {location}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </NextLink>
     );
 }
