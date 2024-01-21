9c6a5666c946e0d41aed50f31f6d97e951db1950
804115 <jkoo9@ucsc.edu>
2024-01-21 01:56:20 -0800

    boooo

--- src/app/create/post/page.tsx
+++ src/app/create/post/page.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Box from "@mui/material/Box"
 import Stack from "@mui/material/Stack"
-import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import { Container, FormControl, FormHelperText, Input, InputLabel } from "@mui/material";
+import { styled } from '@mui/material/styles';
 
 const ariaLabel = { 'aria-label': 'description' };
+
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+  });
+  
 
 export default function Page() {
     return (
@@ -21,7 +36,15 @@ export default function Page() {
 
                 <Input placeholder="Description" inputProps={ariaLabel} />
                 <FormHelperText id="my-helper-text">Enter a description.</FormHelperText>
+
+                <Input placeholder="Location" inputProps={ariaLabel} />
+                <FormHelperText id="my-helper-text">Where are you located?</FormHelperText>
             </Box>
+
+            <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
+                Upload file
+                <VisuallyHiddenInput type="file" />
+            </Button>
 
 
         </Stack>
