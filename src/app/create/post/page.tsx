3d15b8d658d234f1821b80ecd635e65d7083d2d7
804115 <jkoo9@ucsc.edu>
2024-01-21 01:12:27 -0800

    fuck this shit

--- src/app/create/post/page.tsx
+++ src/app/create/post/page.tsx
@@ -1,20 +1,32 @@
 import React from "react";
+import Box from "@mui/material/Box"
+import Stack from "@mui/material/Stack"
+import TextField from '@mui/material/TextField';
 import { Container, FormControl, FormHelperText, Input, InputLabel } from "@mui/material";
+
+const ariaLabel = { 'aria-label': 'description' };
 
 export default function Page() {
     return (
-        <Container>
-            <FormControl>
-                <InputLabel htmlFor="post-title">Title</InputLabel>
-                <Input id="post-title" aria-describedby="title-helper" />
-                <FormHelperText id="title-helper">Enter a title for your post</FormHelperText>
-            </FormControl>
+        <Stack direction="column" alignItems={"flex-start"}>
+            <Box component="form" sx={{
+                '& > :not(style)': { m: 1 },
+                padding:"2rem"
+            }}
+                noValidate
+                autoComplete="off"
+            >
+                <Input placeholder="Title" inputProps={ariaLabel} />
+                <FormHelperText id="my-helper-text">Enter a title.</FormHelperText>
 
-            <FormControl>
-                <InputLabel htmlFor="post-content">Description</InputLabel>
-                <Input id="post-content" aria-describedby="content-helper" />
-                <FormHelperText id="content-helper">Enter a description for your post</FormHelperText>
-            </FormControl>
-        </Container>
+                <Input placeholder="Description" inputProps={ariaLabel} />
+                <FormHelperText id="my-helper-text">Enter a description.</FormHelperText>
+            </Box>
+
+
+        </Stack>
+        
+        
+        
     );
 }
