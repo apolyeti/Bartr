1f08b0b2861018732e56d48437c9ccb6414d4541
804115 <jkoo9@ucsc.edu>
2024-01-20 16:34:37 -0800

    rtet

--- src/components/MainPage/NavBar.tsx
+++ src/components/MainPage/NavBar.tsx
@@ -1,10 +1,60 @@
+"use client"
 import Image from "next/image";
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import AppBar from '@mui/material/AppBar';
 import Typography from '@mui/material/Typography';
+<<<<<<< Updated upstream
 import Box from '@mui/material/Box';
+=======
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+const Search = styled('div')(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    width: '100%',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+}));
+>>>>>>> Stashed changes
 
 
 export default function NavBar() {
@@ -21,8 +71,8 @@ export default function NavBar() {
                 <Image
                     src="/bartrlogo.svg"
                     alt="logo"
-                    width={360} // Adjust the width as needed
-                    height={120} // Adjust the height as needed
+                    width={360} 
+                    height={120} 
                 />
                 {/* Login and Sign Up Buttons (Right) */}
                 <Stack spacing={2} direction="row" sx={{ flex:1, justifyContent: "flex-end"}}>
@@ -33,6 +83,16 @@ export default function NavBar() {
                                 SIGN UP
                             </Button>
                 </Stack>
+
+                <Search>
+                    <SearchIconWrapper>
+                        <SearchIcon/>
+                    </SearchIconWrapper>
+                    <StyledInputBase
+                        placeholder="search..."
+                        inputProps={{ 'aria-label': 'search' }}
+                    />
+                </Search>
             </Stack>
         
             {/* AppBar with Dark Grey Color (Below the Logo) */}
