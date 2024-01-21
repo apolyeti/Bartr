c7f97ac132604becb4b80901c7ff9794a7192c04
804115 <jkoo9@ucsc.edu>
2024-01-20 18:48:41 -0800

    commiting changes

--- src/app/profile/[user]/page.tsx
+++ src/app/profile/[user]/page.tsx
@@ -1,3 +1,13 @@
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { AcUnitOutlined } from '@mui/icons-material';
+
 export default function Page({ params }: { params: { user: string } }) {
-    return <div>UserId: {params.user}</div>
-  }
+    return (
+      <Stack>
+        <div>UserId: {params.user}</div>
+        <AccountCircleIcon/>
+      </Stack>
+    ); 
+}
